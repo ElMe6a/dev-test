0dcb96f651bbb677d9121677866506efb8b58452
Jafeth Leyva <jafeth.leyva@coreteam.mx>
2021-09-20 18:52:39 +0000

    [FIX] Modelos y Vistas

--- cvp/models/__init__.py
+++ cvp/models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-#from . import cvp_genero
+from . import cvp_generos
 from . import cvp_peliculas
-#from . import cvp_studio
+from . import cvp_studio
