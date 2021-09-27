35a13adb57faa0888771d072e3aa666ee7135b12
jafeth leyva <jafeth.leyva@coreteam.mx>
2021-09-27 19:55:40 +0000

    [UPDATE] relaciones

--- cvp/models/cvp_peliculas.py
+++ cvp/models/cvp_peliculas.py
@@ -10,8 +10,9 @@ class Peliculas(models.Model):
     titulo =  fields.Char(string = 'Titulo de la Pelicula', required = True)
     fecha_lanzamiento = fields.Date(string ='Fecha de Lanzamiento') #Falta el Constraint
     longuitud_minutos = fields.Datetime(string = 'Minutos de Pelicula')
-    #director_id = fields.Many2one('res.users', required = True, string = ' Director/ Campo Many2one') # Relacion * - 1
-    #actores_id = fields.One2many('res.users', required = True, string = ' Actores/ Campo One2many') # Relacion 1 - *
+    director_id = fields.Many2one('res.users' ,'cdirector_id', ondelete='cascade') # Relacion * - 1
+    #actores_id = fields.One2many('res.users','cactores_id')
+    #actores_id = fields.One2many('res.users', 'cactores_id') # Relacion 1 - *
     #producto_asociado Crear una realcion *-1 a la tabla compras
     sinopsis = fields.Text(string = 'Sinopsis', required = True)
     costo_pelicula = fields.Float(string = 'Costo', required = True)
